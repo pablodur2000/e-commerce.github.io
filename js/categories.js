36bdb3d4f209a2dcc881c18dc030f283ab698362
lpch20 <lpch20@hotmail.com>
2024-08-20 19:38:14 -0300

    add localStorage token in anothers pages

--- js/categories.js
+++ js/categories.js
@@ -88,6 +88,11 @@ function sortAndShowCategories(sortCriteria, categoriesArray){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    const token = localStorage.getItem("token");
+    if (!token){
+        window.location.href = "login.html";
+    }
+    
     getJSONData(CATEGORIES_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             currentCategoriesArray = resultObj.data
