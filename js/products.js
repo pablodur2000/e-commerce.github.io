1ab4ab5becbc49310461c9efb7b033d93f4bb8ac
sofia-valdez <valdezsofia7@gmail.com>
2024-09-11 18:40:36 -0300

    add slash

--- js/products.js
+++ js/products.js
@@ -61,7 +61,7 @@ const showProducts = (products) => {
         productDiv.style.cursor = "pointer";
         productDiv.addEventListener("click", () => {
           localStorage.setItem("product", JSON.stringify(product));
-          window.location.href = "/product-info.html";
+          window.location.href = "/e-commerce.github.io/product-info.html";
         });
     });
 
